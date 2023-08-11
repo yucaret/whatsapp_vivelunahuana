6c7216c11acf7fe82941b750909dc01644ddc888
unknown <jorge.eduardo@vicenteh.com>
2023-08-11 01:56:49 -0500

    chat gepeto 2023-08-11

--- bk/20230811/app.py
+++ bk/20230811/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/bienvenido', methods=['GET'])
 def  bienvenido():
-    app.logger.info('Bienvenido a gepeto')
+    app.logger.info('Bienvenido')
     app.logger.info(sett.token)
     app.logger.info(sett.whatsapp_token)
     app.logger.info(sett.whatsapp_url)
@@ -15,25 +15,20 @@ def  bienvenido():
 
 @app.route('/webhook', methods=['GET'])
 def verificar_token():
-    print("ingresando a verificar_token")
     try:
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
 
         if token == sett.token and challenge != None:
             app.logger.info('Token correcto')
-            print("saliendo de verificar_token: token correcto")
             return challenge
         else:
-            print("saliendo de verificar_token: token incorrecto")
             return 'token incorrecto', 403
     except Exception as e:
-        print("error en verificar_token: " + str(e))
         return e,403
     
 @app.route('/webhook', methods=['POST'])
 def recibir_mensajes():
-    print("ingresando a recibir_mensajes")
     try: 
         body = request.get_json()
         entry = body['entry'][0]
@@ -58,34 +53,42 @@ def recibir_mensajes():
         #    print('es todo')
         #    services.guardar_conversacion(messageId, number, name, text, timestamp, 'pedido realizado')
         if 'conforme' in str.lower(text):
+            print('>>>>>>>>>>>>>>>> conforme >>>>>>>>>>>>>>>>>>')
             services.guardar_conversacion(messageId, number, name, text, timestamp, 'conformidad')
 
             # 2023-07-30 21:06 (jorge eduardo vicente hernández): renombre de variable
             #jsonPedido = services.generar_respuesta_chatgpt(text, number, True)
             #print('1jsonPedido',jsonPedido)
             diccionarioConforme = services.generar_respuesta_chatgpt(text, number, True)
-            
+            print('diccionarioConforme: ', str(diccionarioConforme))
+
             # 2023-07-30 21:06 (jorge eduardo vicente hernández): se envia mensaje al gerente
             #services.guardar_pedido(jsonPedido, number)
             #data = services.text_Message(number,'Pedido Confirmado, gracias!')
+            print('telefono del gerente: ' + str(sett.celular_gerente))
             dataGerente = services.text_Message_al_gerente(sett.celular_gerente, diccionarioConforme)
+            print('recibe data de gerente ', dataGerente)
             respuesta = services.enviar_Mensaje_whatsapp(dataGerente)
-            
-            data = services.text_Message(number,'Gracias en breve se comunicarán contigo!!!')
-        else:
-            respuestabot = services.generar_respuesta_chatgpt(text, number, False)
-            services.guardar_conversacion(messageId, number, name, text, timestamp, respuestabot)
-            data = services.text_Message(number,respuestabot)
-            
-        respuesta = services.enviar_Mensaje_whatsapp(data)
+            print('se envia data de gerente: ' + str(respuesta))
 
-        print("saliendo de recibir_mensajes: " + str(respuesta))
+            data = services.text_Message(number,'Gracias en breve se comunicarán contigo!!!')
+            print('se genera data de conforme')
+        else:
+            print('>>>>>>>>>>>>>>>> sigue conversando >>>>>>>>>>>>>>>>>>')
+            respuestabot = services.generar_respuesta_chatgpt(text, number, False)
+            print('recibe respuesta de bot')
+            services.guardar_conversacion(messageId, number, name, text, timestamp, respuestabot)
+            print('guarda conversacion')
+            data = services.text_Message(number,respuestabot)
+            print('se genera la data de conversacion')
+
+        respuesta = services.enviar_Mensaje_whatsapp(data)
+        print('se envia mensaje con data de conversacion: ' + str(respuesta))
         ######################################################
         return 'enviado'
 
     except Exception as e:
-        print("error en recibir_mensajes: " + str(e))
-        return 'no enviado '
+        return 'no enviado ' + str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
